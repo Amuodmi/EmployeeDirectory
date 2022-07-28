b111359d704518b1d22b311fb90570a3e56a762e
Michelle <michelle.douma@gmail.com>
2022-07-28 17:46:30 -0400

    fixing seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -8,10 +8,9 @@ CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL, 
-    role_id INTEGER,
     job_title VARCHAR(30) NOT NULL,
     department VARCHAR(30) NOT NULL, 
-    salary DECIMAL NOT NULL, 
+    salary DECIMAL(6 , 2) NOT NULL, 
     reporting_manager INTEGER NOT NULL 
 );
 
@@ -24,6 +23,6 @@ CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY, 
     job_title VARCHAR(30) NOT NULL, 
     role_id INTEGER,
-    department INTEGER NOT NULL,
-    salary DECIMAL
+    department VARCHAR(30) NOT NULL,
+    salary DECIMAL(6 , 2) NOT NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
 -- adding info to be inserted into previously created tables
-INSERT INTO employees (first_name, last_name, role_id, job_title, department, salary, reporting_manager)
+INSERT INTO employees (first_name, last_name, job_title, department, salary, reporting_manager)
 VALUES
-    ('Moiraine', 'Damodred', 002, 'Legal Team Lead', 'Legal', 200,000, 1),
-    ('Lan', 'Mandragoran', 003, 'Lawyer', 'Legal', 180,000, 2),
-    ('Rand', 'AlThor', 004, 'Account Manager', 'Finance', 160,000, 2),
-    ('Perrin', 'Ayabara', 005, 'Lead Engineer', 'Engineering', 150,000, 2),
-    ('Mat', 'Cauthon', 006, 'Software Engineer', 'Engineering', 120,000, 3),
-    ('Egwene', 'AlVere', 007, 'Sales Lead', 'Sales', 100,000, 3),
-    ('Nynaeve', 'AlMeara', 008, 'Salesperson', 'Sales', 80,000, 3);
+    ('Moiraine', 'Damodred', 'Legal Team Lead', 'Legal', 200000, 1),
+    ('Lan', 'Mandragoran', 'Lawyer', 'Legal', 180000, 2),
+    ('Rand', 'AlThor', 'Account Manager', 'Finance', 160000, 2),
+    ('Perrin', 'Ayabara', 'Lead Engineer', 'Engineering', 150000, 2),
+    ('Mat', 'Cauthon', 'Software Engineer', 'Engineering', 120000, 3),
+    ('Egwene', 'AlVere', 'Sales Lead', 'Sales', 100000, 3),
+    ('Nynaeve', 'AlMeara', 'Salesperson', 'Sales', 80000, 3);
 
 INSERT INTO departments (name)
 VALUES
@@ -16,13 +16,13 @@ VALUES
   ('Legal'),
   ('Sales');
 
-INSERT INTO roles (job_title, salary, department, role_id)
+INSERT INTO roles (job_title, role_id, department, salary)
 VALUES
-    ('Sales Lead', 100,000, 'Sales', 100),
-    ('Salesperson', 80,000, 'Sales', 200),
-    ('Lead Engineer', 150,000, 'Engineering', 300),
-    ('Software Engineer', 120,000, 'Engineering', 400),
-    ('Account Manager', 160,000, 'Finance', 500),
-    ('Accountant', $120,000, 'Finance', 600),
-    ('Legal Team Lead', 200,000, 'Legal', 700),
-    ('Lawyer', $180,000, 'Legal', 800);
+    ('Sales Lead', 100, 'Sales', 100000),
+    ('Salesperson', 200, 'Sales', 80000),
+    ('Lead Engineer', 300, 'Engineering', 150000),
+    ('Software Engineer', 400,'Engineering', 120000),
+    ('Account Manager', 500, 'Finance', 160000),
+    ('Accountant', 600, 'Finance', 120000),
+    ('Legal Team Lead', 700, 'Legal', 200000),
+    ('Lawyer', 800, 'Legal', 180000);
